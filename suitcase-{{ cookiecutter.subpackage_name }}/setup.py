543f75dfdc70f60adeb2af1adcba05bccb417260
danielballan <daniel.b.allan@gmail.com>
2018-06-22 11:48:11 -0400

    FIX: typo

--- suitcase-{{ cookiecutter.subpackage_name }}/setup.py
+++ suitcase-{{ cookiecutter.subpackage_name }}/setup.py
@@ -5,6 +5,6 @@ import versioneer
 setup(
     name='suitcase-{{ cookiecutter.subpackage_name }}',
     version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass()
+    cmdclass=versioneer.get_cmdclass(),
     packages=['suitcase.{{ cookiecutter.subpackage_name }}']
 )
