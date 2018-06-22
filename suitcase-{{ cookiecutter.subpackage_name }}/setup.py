293623faeb87034d064eab172511961518f367c5
danielballan <daniel.b.allan@gmail.com>
2018-06-22 15:22:29 -0400

    Add requirements.txt.

--- suitcase-{{ cookiecutter.subpackage_name }}/setup.py
+++ suitcase-{{ cookiecutter.subpackage_name }}/setup.py
@@ -2,9 +2,20 @@ from setuptools import setup
 import versioneer
 
 
+with open('requirements.txt') as f:
+    requirements = f.read().split()
+
+
 setup(
     name='suitcase-{{ cookiecutter.subpackage_name }}',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    packages=['suitcase.{{ cookiecutter.subpackage_name }}']
+    license="BSD (3-clause)",
+    install_requires=requirements,
+    packages=['suitcase.{{ cookiecutter.subpackage_name }}'],
+    classifiers=[
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
 )
