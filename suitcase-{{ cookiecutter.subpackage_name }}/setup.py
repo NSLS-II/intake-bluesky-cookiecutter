0889a2a905a5bce58665feb2e450f06a3906e783
danielballan <daniel.b.allan@gmail.com>
2018-06-22 15:17:26 -0400

    API: Document guidelines and move packages around.

--- suitcase-{{ cookiecutter.subpackage_name }}/setup.py
+++ suitcase-{{ cookiecutter.subpackage_name }}/setup.py
@@ -6,5 +6,5 @@ setup(
     name='suitcase-{{ cookiecutter.subpackage_name }}',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    packages=['suitcase.suitcase_{{ cookiecutter.subpackage_name }}']
+    packages=['suitcase.{{ cookiecutter.subpackage_name }}']
 )
